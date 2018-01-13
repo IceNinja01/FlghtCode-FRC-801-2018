5a9d73e4629139cbe1ea5279469e2b1c2cbf2ebf
Rolando Valdez <rolandov00@gmail.com>
2018-01-13 18:05:14 -0500

    fff

---
+++ src/org/usfirst/frc/team801/robot/subsystems/Pinchers.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team801.robot.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+
+public class Pinchers extends Subsystem {
+	
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
