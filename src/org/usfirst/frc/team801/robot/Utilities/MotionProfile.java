e75a9af5ceb844370030eb44f098ce6f771fdd26
Rolando Valdez <rolandov00@gmail.com>
2018-01-15 16:21:33 -0500

    Worked on MotionProfiel
    Added two classes:
    MotionProfileExample
    Instrumentation

--- src/org/usfirst/frc/team801/robot/Utilities/MotionProfile.java
+++ src/org/usfirst/frc/team801/robot/Utilities/MotionProfile.java
@@ -6,27 +6,75 @@ public class MotionProfile {
 	This method outputs a 3 column array, [position, velocity, ms], is used for a 1D motion control
 	Uses a trapezoidal method for ramp on and ramp on.
 	 1. v = V(t) + Accel;
-	 2. p = P(t) + v + I / 2*Accel;
+	 2. p = P(t) + v + 1 / 2*Accel;
 	 Where V(t) is equal to the motors current velocity
 	 and where P(t) is equal to the motors current position
 	 */
-	public void OneDimenisonMotion(double distance, double maxVel, double accel) {
-		/* 
+	public double[][] OneDimenisonMotion(double distance, double maxVel, double accel) {
+		/* distance is units ft
+		 * MaxVelocity is ft/sec
+		 * accel is ft/sec^2
 		 */
-		
-		//double rampTime = maxVel/accel;
 		
 		double pathTime = 0;
 		double rampTime = maxVel/accel;
-	    double accelDist = 0.5 * maxVel * maxVel / accel;
+	    double accelDist = 0.5 * accel*Math.pow(rampTime, 2);
 	    // Tests if we can even reach our maxVelocity, if we can't, then the boolean is true
 	    if (accelDist * 2 > distance)
 	    {//Triangle
-	        pathTime = rampTime * 2;
+	    	pathTime = Math.sqrt(distance/accel) * 2;
 	    } else
 	    {//Trapezoidal
 	        pathTime = rampTime * 2 + (distance - accelDist * 2) / maxVel;
 	    }
+	    double dt = 0.01; // steps are equal to 10ms
+	    int segments = (int) (pathTime*(1/0.01));
+	    // [position, velocity, time]
+		double[][] path = new double[segments][3];
+		path[0][0] = 0;
+		path[0][1] = 0;
+		path[0][2] = dt;
+		
+		for(int i = 1; i < segments; i++)
+		{
+			//v = V(t) + Accel;
+			double accelCurrent = 0;
+			//Triangle 
+			//Method needs to be tested on the robot to evaluate its precision
+			if(accelDist * 2 > distance)
+			{
+				if(i >= segments/2)
+				{
+					accelCurrent = -accel;
+				}
+				else
+				{
+					accelCurrent = accel;
+				}
+			}
+			//Trapezoidal
+			else
+			{
+				if(i <= rampTime/dt)
+				{
+					accelCurrent = accel;
+				}
+				else if(i > rampTime/dt && i < segments-rampTime/dt)
+				{
+					accelCurrent = 0;
+				}
+				else if(i > segments-rampTime/dt)
+				{
+					accelCurrent = -accel;
+				}
+			}
+
+			path[i][0] = path[i-1][0] + ((path[i-1][1] + (accelCurrent*dt))*dt) + 0.5*accelCurrent*Math.pow(dt,2); //position
+			path[i][1] = path[i-1][1] + (accelCurrent*dt); 								 //velocity
+			path[i][2] = dt;												 //time
+		}
+		
+		return path;
 	    
 	}
 	
---
+++ src/org/usfirst/frc/team801/robot/Utilities/instrumentation.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team801.robot.Utilities;
+
+import com.ctre.CANTalon.MotionProfileStatus;
+
+public class instrumentation {
+
+	public static void OnNoProgress()
+	{
+		
+	}
+
+	public static void process(MotionProfileStatus _status)
+	{
+		
+	}
+
+	public static void OnUnderrun()
+	{
+		
+	}
+
+}
