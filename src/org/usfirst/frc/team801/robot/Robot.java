92074ba670a7987f2838a19e0f67240bc1a06711
Rolando Valdez <rolandov00@gmail.com>
2018-02-05 22:34:01 -0500

    Add SmartDashboard getInstance

--- src/org/usfirst/frc/team801/robot/Robot.java
+++ src/org/usfirst/frc/team801/robot/Robot.java
@@ -46,6 +46,7 @@ public class Robot extends IterativeRobot {
 		m_chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", m_chooser);
+		SmartDashboard.putData(Scheduler.getInstance());
 		oi = new OI();
 	}
 
