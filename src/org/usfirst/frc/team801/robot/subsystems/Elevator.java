def46cb13691db9af876e566852400d80b409c88
Redheadninja <ethansampson0214@gmail.com>
2018-02-10 14:25:58 -0500

    quad encoder

--- src/org/usfirst/frc/team801/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team801/robot/subsystems/Elevator.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team801.robot.subsystems;
 import org.usfirst.frc.team801.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -12,27 +11,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Elevator extends Subsystem {
-
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	
 	private TalonSRX motor = new TalonSRX(9);
-	
-	public void Lifter() {
-		for(int i = 0; i < 2; i++) {
-			motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, Constants.kTimeoutMs);
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public Elevator() {
+    	for(int i=0;i<4;i++){
+			motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, Constants.kTimeoutMs);
 			/* set the peak and nominal outputs, 12V means full */
 			motor.configNominalOutputForward(0, Constants.kTimeoutMs);
 			motor.configNominalOutputReverse(0, Constants.kTimeoutMs);
-			motor.configPeakOutputForward(11.0, Constants.kTimeoutMs);
-			motor.configPeakOutputReverse(-11.0, Constants.kTimeoutMs);
-		}
-	}
-
-
-	public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+			motor.configPeakOutputForward(0.0, Constants.kTimeoutMs);
+			motor.configPeakOutputReverse(0.0, Constants.kTimeoutMs);
+    	}
     }
     
     public void shrink() {
@@ -45,7 +41,7 @@ public class Elevator extends Subsystem {
     }
     
     public void extendHigh() {
-    	//extend to grab rung
+    	
     }
 }
 
