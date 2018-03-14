e1bbb05a577f4428e5764e41f23b336807010dfd
Rolando Valdez <rolandov00@gmail.com>
2018-03-14 19:22:32 -0400

    Nikhil changes

--- src/org/usfirst/frc/team801/robot/commands/auto/nikhil_test/StartPosGoPosSwitch.java
+++ src/org/usfirst/frc/team801/robot/commands/auto/nikhil_test/StartPosGoPosSwitch.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team801.robot.commands.auto.nikhil_test;
 
+import org.usfirst.frc.team801.robot.Constants;
 import org.usfirst.frc.team801.robot.commands.arm.ArmDown;
 import org.usfirst.frc.team801.robot.commands.arm.ArmUp;
 import org.usfirst.frc.team801.robot.commands.chassis.CMD_Angle_Drive;
@@ -10,7 +11,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class StartPosGoPosSwitch extends CommandGroup {
 	
-	public StartPosGoPosSwitch() {
+	public StartPosGoPosSwitch(int direction) {
+		if (direction != Constants.LEFT && direction != Constants.RIGHT)
+			throw new IllegalArgumentException();
 		addSequential(new ArmUp());
     	addSequential(new CMD_Angle_Drive(104, 90, 0.2));
     	addSequential(new ExtendLow());
